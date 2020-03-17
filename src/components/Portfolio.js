a72f6cacd3d8f33583d995810adf9c94c43f4440
Ga Ming Chan <gmchan19@gmail.com>
2020-03-17 17:33:49 +0000

    added info in project cards

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -13,9 +13,9 @@ class Portfolio extends Component {
         projects: [
             { image: calculator, title: "Calculator", link: 'https://gxc19.github.io/calculator', info: 'This is a calculator I have made using React'},
             { image: bongoCat, title: "Bongo Cat", link: 'https://gxc19.github.io/drumKit', info: 'This is a Bongo Cat themed drum kit using a combination of JavaScript Dom, HTML and CSS.'},
-            { image: weatherApp, title: "Weather App", link: 'https://powerful-ridge-89114.herokuapp.com', info: 'yes'},
-            { image: appleClone, title: "Apple Clone", link: 'https://gxc19.github.io/appleWebsiteClone', info: 'yes'},
-            { image: googleFonts, title: "Google Fonts", link: 'https://gxc19.github.io/gfont-clone-challenge', info: 'yes'},
+            { image: weatherApp, title: "Weather App", link: 'https://powerful-ridge-89114.herokuapp.com', info: 'This is a full stack weather application'},
+            { image: appleClone, title: "Apple Clone", link: 'https://gxc19.github.io/appleWebsiteClone', info: 'This a cloned version of Apples website using HTML and CSS'},
+            { image: googleFonts, title: "Google Fonts", link: 'https://gxc19.github.io/gfont-clone-challenge', info: 'This is a cloned version of Google Fonts website using React'},
         ]
     }
     
