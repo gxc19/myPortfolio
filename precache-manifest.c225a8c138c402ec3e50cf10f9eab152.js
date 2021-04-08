2190b4d9f24337712c024da4312bcc79ad3e08c7
Ga Ming Chan <gmchan@gas-macbook-pro.home>
2021-04-08 18:36:38 +0100

    Updates

---
+++ precache-manifest.c225a8c138c402ec3e50cf10f9eab152.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35c476207898d130e3927bf7b355a222",
+    "url": "/myPortfolio/index.html"
+  },
+  {
+    "revision": "708b3b1db1e5b65fc19a",
+    "url": "/myPortfolio/static/css/2.7abef457.chunk.css"
+  },
+  {
+    "revision": "714f74d68f6cf73c0d86",
+    "url": "/myPortfolio/static/css/main.bc880172.chunk.css"
+  },
+  {
+    "revision": "708b3b1db1e5b65fc19a",
+    "url": "/myPortfolio/static/js/2.fd6260b9.chunk.js"
+  },
+  {
+    "revision": "e1628d5e981d35bdceca224eaf12c1ab",
+    "url": "/myPortfolio/static/js/2.fd6260b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "714f74d68f6cf73c0d86",
+    "url": "/myPortfolio/static/js/main.82f86cf5.chunk.js"
+  },
+  {
+    "revision": "5ebb7dfb6390de1b1c37",
+    "url": "/myPortfolio/static/js/runtime-main.866b783a.js"
+  },
+  {
+    "revision": "8ccf66edfeb525fa6d227836460779ef",
+    "url": "/myPortfolio/static/media/appleClone9.8ccf66ed.png"
+  },
+  {
+    "revision": "fafa9d2b1946e4966a011b6155614806",
+    "url": "/myPortfolio/static/media/bongoCat9.fafa9d2b.png"
+  },
+  {
+    "revision": "50a9468afd1af924ef44af43bb142c9c",
+    "url": "/myPortfolio/static/media/calculator9.50a9468a.png"
+  },
+  {
+    "revision": "0b15f3156d0df47309ec3fac44953434",
+    "url": "/myPortfolio/static/media/goGetWork.0b15f315.png"
+  },
+  {
+    "revision": "1ab82029f9f9932ac459f365dc8cef11",
+    "url": "/myPortfolio/static/media/googleFonts9.1ab82029.png"
+  },
+  {
+    "revision": "fbdbaa029ca1e9602f37b556606e186c",
+    "url": "/myPortfolio/static/media/weatherApp9.fbdbaa02.png"
+  }
+]);
